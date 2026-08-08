cc50482b5d3e0f256307306d376862b625f9156b
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-08 16:02:40 -0600

    Camera and DirectionalLight

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,18 @@ fn setup(mut commands: Commands) {
         Transform::from_xyz(0.0, 2.0, 0.0),
     ));
     commands.spawn((RigidBody::Static, Collider::cuboid(5.0, 0.5, 5.0)));
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 5000.0,
+            ..default()
+        },
+        Transform::default().looking_at(Vec3::new(-1.0, -2.5, -1.5), Vec3::Y),
+    ));
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_translation(Vec3::new(20.0, 10.0, 20.0)).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+    println!("done");
 }
 
 fn main() {
