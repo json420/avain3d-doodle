3327975932f74a4a4f8dc5b1550279f36fc63b68
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-08 12:29:03 -0600

    standard bevy app startup

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ fn setup(mut commands: Commands) {
         RigidBody::Dynamic,
         Collider::capsule(0.5, 1.5),
         ColliderDensity(2.0),
+        Transform::default(),
     ));
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
