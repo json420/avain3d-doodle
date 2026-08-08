ff283d23ee2619b5079f5095698f7e4899cc27e7
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-08 13:13:46 -0600

    PhysicsPlugins

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@ use bevy::prelude::*;
 fn setup(mut commands: Commands) {
     commands.spawn((
         RigidBody::Dynamic,
-        Collider::capsule(0.5, 1.5),
+        Collider::sphere(0.5),
         ColliderDensity(2.0),
-        Transform::default(),
+        Transform::from_xyz(0.0, 2.0, 0.0),
     ));
+    commands.spawn((RigidBody::Static, Collider::cuboid(5.0, 0.5, 5.0)));
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .add_systems(Startup, setup)
         .run();
 }
