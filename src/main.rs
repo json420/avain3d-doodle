3c4deb9168653b18434a5b746672f9a04e9b7863
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-08 12:02:02 -0600

    Use avian3d, bevy release crates instead of from git

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use avian3d::prelude::*;
+use bevy::prelude::*;
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::capsule(0.5, 1.5),
+        ColliderDensity(2.0),
+    ));
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
