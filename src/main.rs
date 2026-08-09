541a2bb4dc5a95d3200f0d22a7d56edbf14194c1
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-09 15:30:18 -0600

    unlock y rotation for player

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,22 @@ fn setup(
     ));
 
     // The player
-    commands.spawn((
-        Player,
-        RigidBody::Dynamic,
-        Collider::capsule(0.4, 1.0),
-        Mesh3d(meshes.add(Capsule3d::new(0.4, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
-        Transform::from_xyz(2.0, 2.5, 0.75),
-        LockedAxes::ROTATION_LOCKED,
-    )).with_children(|parent| {
-        parent.spawn((
-            Camera3d::default(),
-            Transform::from_xyz(0.0, 1.0, -9.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ));
-});
+    commands
+        .spawn((
+            Player,
+            RigidBody::Dynamic,
+            Collider::capsule(0.4, 1.0),
+            Mesh3d(meshes.add(Capsule3d::new(0.4, 1.0))),
+            MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+            Transform::from_xyz(2.0, 2.5, 0.75),
+            LockedAxes::ROTATION_LOCKED.unlock_rotation_y(),
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Camera3d::default(),
+                Transform::from_xyz(0.0, 2.0, -9.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ));
+        });
 
     commands.spawn((
         RigidBody::Dynamic,
